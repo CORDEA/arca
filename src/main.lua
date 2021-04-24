a1095618fb8d83afdf877369dd3dd4d76d90ab7a
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-24 14:52:10 +0900

    receive data from stdin

--- src/main.lua
+++ src/main.lua
@@ -2,14 +2,13 @@ DataRepository = require("data_repository")
 
 local function main()
     local command = arg[1]
+    local key = arg[2]
     local password = os.getenv("ARCA_PASSWORD")
     local repository = DataRepository:new({ password = password })
     if command == "add" then
-        local key = arg[2]
-        local data = arg[3]
+        local data = io.read("*all")
         repository.insert(key, data)
     elseif command == "find" then
-        local key = arg[2]
         repository.find(key)
     elseif command == "list" then
         local r = repository.findAll()
