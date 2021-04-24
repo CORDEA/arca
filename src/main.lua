590def1b150abd09d16bf7ec0829e20700115179
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-24 14:54:42 +0900

    assert values

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,16 @@
 DataRepository = require("data_repository")
 
 local function main()
-    local command = arg[1]
+    local command = assert(arg[1])
     local key = arg[2]
-    local password = os.getenv("ARCA_PASSWORD")
+    local password = assert(os.getenv("ARCA_PASSWORD"))
     local repository = DataRepository:new({ password = password })
     if command == "add" then
         local data = io.read("*all")
+        assert(key)
         repository.insert(key, data)
     elseif command == "find" then
+        assert(key)
         repository.find(key)
     elseif command == "list" then
         local r = repository.findAll()
