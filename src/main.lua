de2a5a3ef2408b07b11276c0b0dd281eacd16603
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-24 14:47:47 +0900

    add options

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,21 @@
 DataRepository = require("data_repository")
 
 local function main()
-    local password = arg[1]
-    local key = arg[2]
-    local data = arg[3]
+    local command = arg[1]
+    local password = os.getenv("ARCA_PASSWORD")
     local repository = DataRepository:new({ password = password })
+    if command == "add" then
+        local key = arg[2]
+        local data = arg[3]
+        repository.insert(key, data)
+    elseif command == "find" then
+        local key = arg[2]
+        repository.find(key)
+    elseif command == "list" then
+        repository.findAll()
+    else
+        error("Received unknown option.")
+    end
 end
 
 main()
